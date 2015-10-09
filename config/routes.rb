16177f870095b9e1cb03e93dee22fcfbc3be5a88
Chris Wells <chris@folklabs.com>
2015-10-09 19:44:20 +0100

    Landing Page
    Added landing page file
    Added new styles and content]
    Please check config/routes.rb for routing code...

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	# get 'containers/missed', to: 'missed_bins#show'
   resources :containers
   resources :services
-  
+
   get 'missed-containers', to: 'missed_bins#show'
 
   # resource 'missed_bin'
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
 
   get 'styleguide', to: 'visitors#styleguide'
+  get 'landingpage', to: 'visitors#landingpage'
 end
